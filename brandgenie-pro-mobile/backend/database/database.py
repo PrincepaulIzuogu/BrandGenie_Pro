083d5277d5e891d310b58a5c87054257f5e45ee9
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2025-05-14 11:41:06 +0200

    Update database.py

--- brandgenie-pro-mobile/backend/database/database.py
+++ brandgenie-pro-mobile/backend/database/database.py
@@ -1,11 +1,14 @@
-# backend/database/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:THD111@localhost:5432/onebuttonapp"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://mobileuser:mobilepass@mobile-db:5432/mobiledb"
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
