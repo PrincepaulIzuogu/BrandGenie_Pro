6c93746d7f014fa4e5f95b8f2fcbefe8ead5447a
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2025-07-09 22:10:50 +0200

    Update axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://brandgenie-backend-ene6c9htgcauegg3.westeurope-01.azurewebsites.net/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export default api;
-
