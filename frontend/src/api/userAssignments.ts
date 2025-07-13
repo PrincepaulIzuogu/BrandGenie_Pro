ddc149e9df9417e394e26450f60db83fce110532
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2025-07-13 02:45:13 +0200

    Update userAssignments.ts

--- frontend/src/api/userAssignments.ts
+++ frontend/src/api/userAssignments.ts
@@ -2,5 +2,5 @@ import api from './axios';
 
 export const getAllGroups = () => api.get('/groups');
 export const getAllProjects = () => api.get('/projects');
-export const getAllTools = () => api.get('/tools');
+export const getAllTools = () => api.get('/tools/');
 export const getAllFilesByCompanyId = (companyId: number) => api.get(`/drive/files/${companyId}`);
